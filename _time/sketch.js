3d1ee9b34d5f58538f4d3dbbe48447b81a44a1aa
rebleo <rml444@nyu.edu>
2017-02-25 18:04:17 -0500

    adding timeApp

---
+++ _time/sketch.js
@@ -0,0 +1,37 @@
+var aSecond = 1000;
+var theTime = [];
+var theClock;
+
+
+function preload() {
+    theclock = loadImage('deviceAlert.png');
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight)
+
+}
+
+// here is init!
+setInterval(countTime, aSecond);
+
+function countTime() {
+    theClock = new makeTime(theclock, random(-100, width), random(-100, height));
+    theClock.appear();
+
+}
+
+function makeTime(emoji, x, y) {
+    this.x = x;
+    this.y = y;
+    this.emoji = emoji;
+
+    this.appear = function() {
+        image(this.emoji, this.x, this.y)
+    }
+
+    this.fall = function() {
+        this.y = windowHeight;
+    }
+
+}
